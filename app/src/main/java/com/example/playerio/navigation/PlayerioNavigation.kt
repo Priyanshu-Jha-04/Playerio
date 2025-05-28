cfb7d9f90c6b33bfb5c69945436818150a219c5d
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-05-29 02:46:08 +0530

    Update PlayerioNavigation.kt

--- app/src/main/java/com/example/playerio/navigation/PlayerioNavigation.kt
+++ app/src/main/java/com/example/playerio/navigation/PlayerioNavigation.kt
@@ -1,24 +1,29 @@
 package com.example.playerio.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.playerio.MainScreen
+import com.example.playerio.screens.authentication.AuthCheck
 import com.example.playerio.screens.authentication.SignInScreen
 import com.example.playerio.screens.authentication.SignUpScreen
 import com.example.playerio.screens.home.HomeScreen
 import com.example.playerio.screens.landing.LandingScreen01
 import com.example.playerio.screens.landing.LandingScreen02
 import com.example.playerio.screens.landing.LandingScreen03
+import com.example.playerio.screens.library.LibraryScreen
 import com.example.playerio.screens.playing.PlayingScreen
-import com.example.playerio.screens.settings.SettingsScreen
+import com.example.playerio.screens.settings.SearchScreen
 import com.example.playerio.screens.splash.SplashScreen
 
 @Composable
 fun PlayerioNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = PlayerioScreens.LandingScreen01.name) {
+        composable(PlayerioScreens.AuthCheck.name) {
+            AuthCheck(navController = navController)
+        }
         composable(PlayerioScreens.SplashScreen.name) {
             SplashScreen(navController = navController)
         }
@@ -43,8 +48,14 @@ fun PlayerioNavigation() {
         composable(PlayerioScreens.PlayingScreen.name) {
             PlayingScreen(navController = navController)
         }
-        composable(PlayerioScreens.SettingsScreen.name) {
-            SettingsScreen(navController = navController)
+        composable(PlayerioScreens.SearchScreen.name) {
+            SearchScreen(navController = navController)
+        }
+        composable(PlayerioScreens.LibraryScreen.name) {
+            LibraryScreen(navController = navController)
+        }
+        composable(PlayerioScreens.MainScreen.name) {
+            MainScreen()
         }
     }
 }
