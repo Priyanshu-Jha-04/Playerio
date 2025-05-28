31fa31a13df5a720d28375379a3539b091f0e84f
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-05-28 20:29:17 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/example/playerio/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/playerio/screens/home/HomeScreen.kt
@@ -5,11 +5,21 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text("Hello")
+    Surface(modifier = Modifier.fillMaxSize(),
+        ) {
+
     }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    val navController = rememberNavController()
+    HomeScreen(navController = navController)
 }
