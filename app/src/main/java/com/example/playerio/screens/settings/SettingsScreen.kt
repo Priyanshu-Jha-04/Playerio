93309d34c994bf348030dcd9e3df982069b36e1f
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-05-29 02:46:36 +0530

    Delete SettingsScreen.kt

--- app/src/main/java/com/example/playerio/screens/settings/SettingsScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.playerio.screens.settings
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun SettingsScreen(navController: NavController) {
-
-}
